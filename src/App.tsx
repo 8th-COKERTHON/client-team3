ee80391ba33e795f8e1eb7ba54f89db27f8aa1ca
YoungI422 <dlsrudtj@gmail.com>
2026-07-11 06:33:41 +0900

    faat: 집안일 추가 바텀시트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import ChoreCatalogBottomSheet from "./components/chore/ChoreCatalogBottomSheet";
+
 function App() {
   return (
     <main>
       <h1 className="text-headline font-bold">TEAM-3</h1>
+      <ChoreCatalogBottomSheet />
     </main>
   );
 }
